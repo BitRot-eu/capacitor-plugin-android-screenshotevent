eeb0cd84a1ba5be3184a3b5d39ec6da820ad668f
René Thomas Lottes <rl@bitrot.eu>
2022-10-23 10:42:59 +0200

    Updated definitions.ts

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,9 +1,13 @@
 import type { PluginListenerHandle } from '@capacitor/core';
+
+export interface ScreenshotEvent { }
+export type ScreenshotListener = (state: ScreenshotEvent) => void;
+
+
 export interface ScreenshotEventPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
 
   addListener(
     eventName: 'screenshot',
-    listenerFunc: (orientation: { type: OrientationType }) => void,
-  ): Promise<PluginListenerHandle> & PluginListenerHandle;
+    listenerFunc: ScreenshotListener): Promise<PluginListenerHandle> & PluginListenerHandle;
 }
